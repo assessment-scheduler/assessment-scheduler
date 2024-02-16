c009941598aebda58fa8e31c8ad86e375c5b3276
vanessa <98173531+vnsa-abn@users.noreply.github.com>
2024-02-16 16:45:14 +0000

    models added

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import *
-from .admin import *
-from .lecturer import *
-from .courseLecturer import *
-from .course import *
--- models/courseLecturer.py
+++
@@ -1,7 +0,0 @@
-from App.database import db
-
-class courseLecturer(db.Model):
-  __tablename__ = 'courseLecturer'
-
-  u_ID = db.Column(db.Integer, db.ForeignKey('lecturer.u_ID'), nullable=False)
-  courseCode = db.Column(db.String(120), db.ForeignKey('course.courseCode'), nullable=False)
