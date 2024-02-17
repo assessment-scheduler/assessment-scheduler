f24998edf1431e59e36feafc1f2c20c496ab5fc4
vanessa <98173531+vnsa-abn@users.noreply.github.com>
2024-02-17 12:04:38 +0000

    updated dependencies

--- App/default_config.py
+++ App/default_config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db" #need to change
+SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db" #sqlite by default
 SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 7
 ENV = "DEVELOPMENT"
--- database.py
+++
@@ -1,6 +0,0 @@
-from flask import flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-db = SQLAlchemy(app)
-
