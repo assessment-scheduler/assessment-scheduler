a626965238c3264bc2a182fdf8b09630b4e5d2f6
vnsa-abn <vanessa.aubin@hotmail.com>
2024-02-27 21:04:56 +0000

    cli run#1

--- App/models/course.py
+++ App/models/course.py
@@ -9,7 +9,7 @@ class Course(db.Model):
   level = db.Column(db.Integer, nullable=False)
   semester = db.Column(db.Integer, nullable=False)
   aNum = db.Column(db.Integer, nullable=False, default=0)
-  preReqs = db.Column(db.Course, nullable = True)
+  preReqs = db.Column(db.ForeignKey('course.courseCode'))
   #Relationship between a course and programmes to define which programmes a course belongs to
   p_ID = db.Column(db.Integer, db.ForeignKey('programme.p_ID'), nullable = False)
   #creates reverse relationship from Course back to Assessment to access assessments for a specific course
