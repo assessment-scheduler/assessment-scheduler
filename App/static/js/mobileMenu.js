112c33e9f76a20389bb76fb5f409e43d09bae3d8
vsuwi <victor.solanovillalba@my.uwi.edu>
2024-03-13 22:27:07 -0400

    - cleaned up majority of html and css - created js filder to hold all js files

---
+++ App/static/js/mobileMenu.js
@@ -0,0 +1,34 @@
+const navbarToggler = document.querySelector(".burger");
+    const navbarMenu = document.querySelector(".links_container_mobile");
+    const navbarLinks = document.querySelectorAll(".mobile_links");
+    const exit = document.querySelector(".exit");
+
+var tl = gsap.timeline({ defaults: { duration: 1, ease: Expo.easeInOut } })
+navbarToggler.addEventListener("click", navbarTogglerClick);
+tl.paused(true)
+tl.to(".links_container_mobile", {
+  height: "100%"
+});
+tl.to("#layout_nav_links_mobile li", {
+  opacity: 1,
+  stagger: 0.1
+}, "-=1");
+tl.to("#layout_nav_links_mobile", {
+  pointerEvents: "all"
+}, "-=1");
+tl.to(".exit", {
+  opacity: 1,
+  y: "5px",
+  pointerEvents: "all"
+}, "-=1");
+function navbarTogglerClick() {
+  tl.play();
+};
+exit.addEventListener('click', () => {
+  tl.reverse(.9);
+});
+navbarLinks.forEach(item => {
+  item.addEventListener('click', () => {
+    tl.reverse(.9);
+  })
+});
