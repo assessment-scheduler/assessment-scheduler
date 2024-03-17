41a1a8ea1e0fdf8d697304bc749fd32b5c598635
vnsa-abn <vanessa.aubin@hotmail.com>
2024-03-17 16:51:14 +0000

    routes update

--- App/controllers/course.py
+++ App/controllers/course.py
@@ -1,14 +1,16 @@
 from App.models import Course
 from App.database import db
+import csv
 
 def add_Course(courseCode, courseTitle, description, level, semester, aNum):
-    #Check if courseCode is already in db ie. course already added
+    # Check if courseCode is already in db ie. course already added
     course = db.session.query(Course).filter(Course.courseCode == courseCode).count
 
     if course == 0:
         newCourse = Course.addCourse(courseCode, courseTitle, description, level, semester, aNum)
         return newCourse
-    return None
+    return None        
+
 
 def list_Course():
     # Get each object and return as string
--- App/views/admin.py
+++ App/views/admin.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, request, jsonify, render_template, redirect, url_for
-from App.controllers import course
+from App.controllers import Course
 from App.database import db
 from werkzeug.utils import secure_filename
 import os, csv
 #from flask_jwt_extended import current_user as jwt_current_user
 #from flask_jwt_extended import jwt_required
+
+from App.controllers.course import (
+    add_Course
+)
 
 admin_views = Blueprint('admin_views', __name__, template_folder='../templates')
  
@@ -49,21 +53,17 @@ def upload_course_file():
         else:
             # Secure filename
             filename = secure_filename(file.filename)
-
+        
             # Save file to uploads folder
             file.save(os.path.join('App/uploads', filename)) 
-
+            
             # Retrieves course details from file and stores it in database ie. store course info 
             fpath = 'App/uploads/' + filename
             with open(fpath, 'r') as file:
                 reader = csv.DictReader(file)
                 for row in reader:
                     #create object
-                    new_course = course.add_Course(courseCode=row['course code'], courseTitle=row['title'], description=row['description'], level=row['level'], semester=row['sem'], aNum=row['aNum'])
-                    # else: ensures courses are available for semester selected
-                    #     message = row['course code'] + ' is not offered in Semester 2' + ' ! Please edit Course Listings.'
-                    #     return render_template('test.html', message = message)    
+                    course = add_Course(courseCode=row['course code'], courseTitle=row['title'], description=row['description'], level=row['level'], semester=row['sem'], aNum=row['aNum'])
 
             # Return course listings!        
             return render_template('courses.html')     
-    
