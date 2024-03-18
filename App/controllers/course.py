4a8b97b26a026f20a04a8077d5ae8cbf634044a2
vnsa-abn <vanessa.aubin@hotmail.com>
2024-03-18 17:18:10 +0000

    routes update

--- App/controllers/course.py
+++ App/controllers/course.py
@@ -1,16 +1,18 @@
 from App.models import Course
 from App.database import db
-import csv
 
 def add_Course(courseCode, courseTitle, description, level, semester, aNum):
     # Check if courseCode is already in db ie. course already added
-    course = db.session.query(Course).filter(Course.courseCode == courseCode).count
-
-    if course == 0:
-        newCourse = Course.addCourse(courseCode, courseTitle, description, level, semester, aNum)
+    course = Course.query.get(courseCode)
+    if course:
+        return course
+    else:
+         #Add new Course
+        newCourse = Course(courseCode, courseTitle, description, level, semester, aNum)
+        db.session.add(newCourse)  #add to db
+        db.session.commit()
         return newCourse
     return None        
-
 
 def list_Course():
     # Get each object and return as string
