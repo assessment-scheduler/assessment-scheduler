14cc926b733d3259ad9c8963e5ec58e03a91ba1b
vnsa-abn <vanessa.aubin@hotmail.com>
2024-04-14 14:38:12 +0000

    attempt to setup email

--- App/send_email.py
+++ App/send_email.py
@@ -1,20 +1,20 @@
-import smtplib
+# import smtplib
 
-# Takes veryy long but works
-def send_mail(email):
-    smtp_server = 'smtp.gmail.com'
-    sender_email = 'vanessa.onica@gmail.com'
-    receiver_email = email
-    appPassword = 'urbskwoytvlmzowc'
-    message = 'This is a test email!'
+# # Takes veryy long but works
+# def send_mail(email):
+#     smtp_server = "smtp-mail.outlook.com"
+#     sender_email = 'vanessa.aubin@hotmail.com'
+#     receiver_email = email
+#     appPassword = 'urbskwoytvlmzowc'
+#     message = 'This is a test email!'
 
-    # Send email
-    with smtplib.SMTP(smtp_server, 587) as server:
-        server.ehlo()
-        server.starttls()
-        server.login(sender_email, appPassword)
-        server.sendmail(sender_email, receiver_email, message)
-        server.close()     
+#     # Send email
+#     with smtplib.SMTP(smtp_server, 587) as server:
+#         server.ehlo()
+#         server.starttls()
+#         server.login(sender_email, appPassword)
+#         server.sendmail(sender_email, receiver_email, message)
+#         server.close()     
 
 
     # Flask mail configs
