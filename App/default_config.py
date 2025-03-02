720e41e785453be6fef1c903e50b7eafa76a54bc
arashi009 <brandonchandoo66@gmail.com>
2025-03-02 13:18:37 -0400

    updated gitignore and config

--- App/default_config.py
+++ App/default_config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://root:MVGy1YesHlfTRDab5Bj2r4fckCWHf3pe@dpg-co5jbju3e1ms73b7a2k0-a.oregon-postgres.render.com/scheduler_db_t28q"
+SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
 SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 7
 ENV = "DEVELOPMENT"
