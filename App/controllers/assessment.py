6ccddeaf786005ec8f09e1cef588b3a283b58d28
Mark <74921616+markfmyt@users.noreply.github.com>
2025-03-15 14:42:00 -0400

    Fixed the dates after it has been scheduled (#20)

--- App/controllers/assessment.py
+++ App/controllers/assessment.py
@@ -5,34 +5,45 @@ from ..controllers.staff import get_staff_courses
 from ..models.assessment import Assessment
 from ..database import db
 
-def get_assessment(course_code, name)-> Optional[Assessment]:
+
+def get_assessment(course_code, name) -> Optional[Assessment]:
     return Assessment.query.filter_by(course_code=course_code, name=name).first()
+
 
 def get_assessment_by_id(id) -> Optional[Assessment]:
     return Assessment.query.get(id)
 
+
 def get_all_assessments() -> List[Assessment]:
     return Assessment.query.all()
 
-def get_num_assessments(course_code:str) -> int:
+
+def get_num_assessments(course_code: str) -> int:
     assessments = get_assessments_by_course(course_code)
     return len(assessments)
 
-def get_assessment_dictionary_by_course(course_code) -> Dict[str,List[Assessment]]:
-    course_assessments: List[Assessment] = Assessment.query.filter_by(course_code = course_code).all()
+
+def get_assessment_dictionary_by_course(course_code) -> Dict[str, List[Assessment]]:
+    course_assessments: List[Assessment] = Assessment.query.filter_by(
+        course_code=course_code
+    ).all()
     if course_assessments == []:
-        return {'code':course_code, 'assessments':[]}
-    json_list: List[dict[str, int]] = [assessment.to_json() for assessment in course_assessments]
-    course_dict: dict[str, Any] = {'code' : course_code, 'assessments': json_list}
+        return {"code": course_code, "assessments": []}
+    json_list: List[dict[str, int]] = [
+        assessment.to_json() for assessment in course_assessments
+    ]
+    course_dict: dict[str, Any] = {"code": course_code, "assessments": json_list}
     return course_dict
 
+
 def get_assessments_by_course(course_code: str) -> List[Assessment]:
-    return Assessment.query.filter_by(course_code = course_code).all()
+    return Assessment.query.filter_by(course_code=course_code).all()
+
 
 def get_assessments_by_lecturer(staff_email: str) -> List[Assessment]:
     staff_courses = get_staff_courses(staff_email)
     course_codes = get_course_codes(staff_courses)
-    
+
     all_assessments = []
     course_assessments_map = {}
     for course_code in course_codes:
@@ -41,17 +52,46 @@ def get_assessments_by_lecturer(staff_email: str) -> List[Assessment]:
     return all_assessments
 
 
-def create_assessment(course_code:str, name:str, percentage:int, start_week:int, start_day:int, end_week:int, end_day:int, proctored:int)-> bool:
-    assessment: Optional[Assessment] =  get_assessment(course_code,name)
+def create_assessment(
+    course_code: str,
+    name: str,
+    percentage: int,
+    start_week: int,
+    start_day: int,
+    end_week: int,
+    end_day: int,
+    proctored: int,
+) -> bool:
+    assessment: Optional[Assessment] = get_assessment(course_code, name)
     if assessment:
         return False
-    newassessment = Assessment(course_code=course_code, name=name, percentage=percentage, start_week=start_week, start_day=start_day, end_week=end_week, end_day=end_day, proctored=proctored)
+    newassessment = Assessment(
+        course_code=course_code,
+        name=name,
+        percentage=percentage,
+        start_week=start_week,
+        start_day=start_day,
+        end_week=end_week,
+        end_day=end_day,
+        proctored=proctored,
+    )
     db.session.add(newassessment)
     db.session.commit()
     return True
 
-def update_assessment(id:str, name:str, percentage:int, start_week:int, start_day:int, end_week:int, end_day:int, proctored:int, scheduled:str = None)-> bool:
-    assessment: Optional[Assessment] =  Assessment.query.get(id)
+
+def update_assessment(
+    id: str,
+    name: str,
+    percentage: int,
+    start_week: int,
+    start_day: int,
+    end_week: int,
+    end_day: int,
+    proctored: int,
+    scheduled: str = None,
+) -> bool:
+    assessment: Optional[Assessment] = Assessment.query.get(id)
     if assessment is None:
         return False
     assessment.name = name
@@ -67,22 +107,38 @@ def update_assessment(id:str, name:str, percentage:int, start_week:int, start_da
     return True
 
 
-def schedule_assessment(semester,schedule_date:int,course_code:str,name:str)-> bool:
-    assessment: Optional[Assessment] = get_assessment(course_code,name)
+def schedule_assessment(
+    semester, schedule_date: int, course_code: str, name: str
+) -> bool:
+    assessment: Optional[Assessment] = get_assessment(course_code, name)
+    if not assessment:
+        return False
+
+    days_diff = (schedule_date - semester.start_date).days
+    week = (days_diff // 7) + 1
+    day = (days_diff % 7) + 1
+
     assessment.scheduled = schedule_date
+    assessment.start_week = week
+    assessment.start_day = day
+    assessment.end_week = week
+    assessment.end_day = day
+
     db.session.commit()
     return True
 
+
 def delete_assessment(course_code, name) -> bool:
-    assessment: Optional[Assessment] =  get_assessment(course_code,name)
+    assessment: Optional[Assessment] = get_assessment(course_code, name)
     if assessment is None:
         return False
     db.session.delete(assessment)
     db.session.commit()
     return True
 
+
 def delete_assessment_by_id(assessment_id) -> bool:
-    assessment: Optional[Assessment] =  Assessment.query.get(int(assessment_id))
+    assessment: Optional[Assessment] = Assessment.query.get(int(assessment_id))
     if assessment is None:
         return False
     db.session.delete(assessment)
--- App/views/kris.py
+++ App/views/kris.py
@@ -16,8 +16,9 @@ from ..controllers import (
     get_all_course_codes,
     get_all_courses,
     get_active_semester,
-    get_semester_duration
+    get_semester_duration,
 )
+
 
 def compile_course_data() -> List:
     course_assessment_list: list = []
@@ -27,47 +28,70 @@ def compile_course_data() -> List:
         course_assessment_list.append(assessments)
     return course_assessment_list
 
+
 def compile_class_matrix() -> List[List[int]]:
     course_list: List[str] = get_all_course_codes()
     matrix: List[List[int]] = get_course_matrix(course_list)
     return matrix
+
 
 def compute_schedule():
     semester = get_active_semester()
     if semester is None:
         print("No active semester found")
         return None
-    
+
     courses: List = compile_course_data()
     matrix: List[List[int]] = compile_class_matrix()
     phi_matrix: List[List[int]] = get_phi_matrix(matrix)
-    
+
     k: int = get_semester_duration(semester.id)
     d = semester.max_assessments
     M: int = semester.constraint_value
 
     U_star, _, _ = solve_stage1(courses, matrix, k, M)
-    schedule,Y_star, probability = solve_stage2(courses,matrix,phi_matrix,U_star,k,d,M)
+    schedule, Y_star, probability = solve_stage2(
+        courses, matrix, phi_matrix, U_star, k, d, M
+    )
     print_schedule(schedule, U_star, d, probability)
     return schedule
+
 
 def schedule_all_assessments(schedule):
     semester = get_active_semester()
     if not semester:
         print("Could not schedule assessments, no active semester")
-        return 
+        return
     for row in schedule:
-        code = row[3]
-        name = row[4].split('-')[0]
-        schedule_date = semester.start_date + timedelta(days=int(row[0]))
-        schedule_assessment(semester, schedule_date,code,name)
+        k, week, day, code, assessment_info = row
+        name = assessment_info.split("-")[0]
+        schedule_date = semester.start_date + timedelta(days=(k - 1))
+        schedule_assessment(semester, schedule_date, code, name)
 
 
-kris_views = Blueprint('kris', __name__, template_folder ='../templates')
+kris_views = Blueprint("kris", __name__, template_folder="../templates")
 
-@kris_views.route('/schedule', methods=['POST'])
+
+@kris_views.route("/schedule", methods=["POST"])
 @jwt_required(Staff)
 def get_schedule_action():
     schedule = compute_schedule()
     schedule_all_assessments(schedule)
-    return redirect(url_for('assessment.assessments'))
+    return redirect(url_for("assessment.assessments"))
+
+
+@kris_views.route("/solve", methods=["GET", "POST"])
+@jwt_required(Staff)
+def solve_schedule():
+    schedule = compute_schedule()
+    if schedule:
+        return redirect(url_for("kris.solve_done", schedule_data="solved"))
+    else:
+        return redirect(url_for("admin_views.get_admin_dashboard_page"))
+
+
+@kris_views.route("/solve-done", methods=["GET", "POST"])
+@jwt_required(Staff)
+def solve_done():
+    flash("Schedule solved successfully", "success")
+    return redirect(url_for("admin_views.get_admin_dashboard_page"))
